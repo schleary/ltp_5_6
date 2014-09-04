54160ca07126aab840b3dc741d79750aab833669
Holly Leary <schleary@gmail.com>
2014-09-03 23:37:50 -0700

    Update spelling

--- ltp_5_6.rb
+++ ltp_5_6.rb
@@ -2,6 +2,6 @@ puts "Hello there. What is your first name?"
 first = gets.chomp
 puts "and your middle name?"
 middle = gets.chomp
-puts "How 'bout that last name?"
+puts "How about that last name?"
 last = gets.chomp
 puts "Well, hello ", first, " ", middle, " ", last, ". Thank you for your cooperation!"
